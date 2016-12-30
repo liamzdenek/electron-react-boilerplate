04f94d9e3e55d2f9a3277622c8b344d864b8e80f
Liam Zdenek <liamzdenek@gmail.com>
2016-12-30 14:23:05 -0600

    Adding missing deps, fixing broken routing

--- app/index.js
+++ app/index.js
@@ -19,7 +19,7 @@ import 'file?name=[name].[ext]!./.htaccess';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
+import { applyRouterMiddleware, Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import FontFaceObserver from 'fontfaceobserver';
 import { useScroll } from 'react-router-scroll';
@@ -47,17 +47,17 @@ openSansObserver.load().then(() => {
 import { translationMessages } from './i18n';
 
 // Create redux store with history
-// this uses the singleton browserHistory provided by react-router
+// this uses the singleton hashHistory provided by react-router
 // Optionally, this could be changed to leverage a created history
-// e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
+// e.g. `const hashHistory = useRouterHistory(createBrowserHistory)();`
 const initialState = {};
-const store = configureStore(initialState, browserHistory);
+const store = configureStore(initialState, hashHistory);
 
 // Sync history and store, as the react-router-redux reducer
 // is under the non-default key ("routing"), selectLocationState
 // must be provided for resolving how to retrieve the "route" in the state
 import { selectLocationState } from 'containers/App/selectors';
-const history = syncHistoryWithStore(browserHistory, store, {
+const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState: selectLocationState(),
 });
 
